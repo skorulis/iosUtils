ade878b4e7b0444b391be9d2560beeb1935c0376
Alex Skorulis <alex.skorulis@thomsonreuters.com>
2013-05-31 21:32:40 +0100

    Got sick of having a separate vector class and made it possible to just perform vector operations on points.

---
+++ math/CGPointMath.h
@@ -0,0 +1,17 @@
+//
+//  CGPointMath.h
+//  phringly
+//
+//  Created by Alex Skorulis on 31/05/2013.
+//  Copyright (c) 2013 Thomson Reuters. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CGPointMath : NSObject
+
++ (CGFloat) dist:(CGPoint)p1 p2:(CGPoint)p2;
++ (CGPoint) normalise:(CGPoint)p1;
++ (CGFloat) length:(CGPoint)p1;
+
+@end
---
+++ math/CGPointMath.m
@@ -0,0 +1,28 @@
+//
+//  CGPointMath.m
+//  phringly
+//
+//  Created by Alex Skorulis on 31/05/2013.
+//  Copyright (c) 2013 Thomson Reuters. All rights reserved.
+//
+
+#import "CGPointMath.h"
+
+@implementation CGPointMath
+
++ (CGFloat) dist:(CGPoint)p1 p2:(CGPoint)p2 {
+    CGFloat f = (p1.x - p2.x)*(p1.x - p2.x) + (p1.y - p2.y)*(p1.y - p2.y);
+    return sqrtf(f);
+}
+
++ (CGPoint) normalise:(CGPoint)p1 {
+    CGFloat len = [self length:p1];
+    if(len == 0) { return CGPointMake(0, 0); }
+    return CGPointMake(p1.x/len, p1.y/len);
+}
+
++ (CGFloat) length:(CGPoint)p1 {
+    return sqrt(p1.x*p1.x + p1.y*p1.y);
+}
+
+@end
