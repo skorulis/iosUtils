37a0b2acc7eedae9e5299dceeeef04d15f150b8e
Alex Skorulis <skorulis@gmail.com>
2013-06-10 23:34:36 +0100

    Finished the skorulis.com logo

--- drawing/Graphics.m
+++ drawing/Graphics.m
@@ -10,37 +10,61 @@
 #import <QuartzCore/QuartzCore.h>
 #import "CGPointMath.h"
 
+#define SPIKE_RADIANS M_PI / 5
+#define RIGHT_CIRCLE_RATIO 0.43f
+#define LEFT_CIRCLE_RATIO 0.25f
+#define SPIKE_RATIO 0.51f
+#define GAP_RATIO 0.03f
+
 @implementation Graphics
 
 + (CGPathRef) newSkorulisLogoPath:(CGFloat)size {
-    float rightCircleRatio = 0.4f;
-    float leftCircleRatio = 0.2f;
-    float gapRatio = 0.05f;
-    float spikeRadians = M_PI / 12;
-    float rightCircleSize = rightCircleRatio*size;
-    float leftCircleSize = leftCircleRatio*size;
-    float gapSize = gapRatio * size;
-    float spikeRadius = rightCircleSize/2 + gapSize;
-    float spikeSize = 0.3f*size;
+    float rightCircleSize = RIGHT_CIRCLE_RATIO*size;
+    float leftCircleSize = LEFT_CIRCLE_RATIO*size;
     
-    CGFloat rCirclePoint = size-rightCircleSize/2;
+    
     
     CGMutablePathRef path = CGPathCreateMutable();
     CGPathAddEllipseInRect(path, NULL, CGRectMake(size-rightCircleSize, size-rightCircleSize, rightCircleSize, rightCircleSize));
     
     CGPathAddEllipseInRect(path, NULL, CGRectMake(0, 0, leftCircleSize, leftCircleSize));
     
-    CGPoint dir = [CGPointMath pointFromAngle:-M_PI];
+    [self drawSpike:path angle:-3*M_PI_4 size:size];
+    [self drawSpike:path angle:-M_PI size:size];
+    [self drawSpike:path angle:-M_PI_2 size:size];
     
-    float x = rCirclePoint + dir.x*spikeRadius;
-    float y = rCirclePoint + dir.y*spikeRadius;
     
+    return path;
+}
+
++ (void) drawSpike:(CGMutablePathRef)path angle:(CGFloat)angle size:(CGFloat)size {
+    float gapSize = GAP_RATIO * size;
+    float spikeRadius = RIGHT_CIRCLE_RATIO*size*0.5 + gapSize;
+    float spikeSize = SPIKE_RATIO*size;
+    
+    CGFloat rCirclePoint = size-RIGHT_CIRCLE_RATIO*size*0.5;
+    
+    CGPoint dir1 = [CGPointMath pointFromAngle:(angle-(SPIKE_RADIANS/2))];
+    CGPoint dir = [CGPointMath pointFromAngle:(angle)];
+    
+    float x = rCirclePoint + dir1.x*spikeRadius;
+    float y = rCirclePoint + dir1.y*spikeRadius;
     
     CGPathMoveToPoint(path, NULL, x, y);
+    
+    CGPathAddRelativeArc(path, NULL, rCirclePoint, rCirclePoint, spikeRadius, (angle-SPIKE_RADIANS/2), SPIKE_RADIANS);
+    
     x = rCirclePoint + dir.x*(spikeRadius+spikeSize);
     y = rCirclePoint + dir.y*(spikeRadius+spikeSize);
+    
     CGPathAddLineToPoint(path, NULL, x, y);
-    return path;
+    
+    x = rCirclePoint + dir1.x*spikeRadius;
+    y = rCirclePoint + dir1.y*spikeRadius;
+    
+    
+    CGPathAddLineToPoint(path, NULL, x, y);
+    
 }
 
 @end
